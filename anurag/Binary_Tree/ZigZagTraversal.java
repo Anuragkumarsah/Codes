e9aeedf5f8f22808b9c6f5fac51e2cc9f08cd624
Anuragkumarsah <asah2307@gmail.com>
2022-07-24 20:05:38 +0530

    Add all codes

---
+++ anurag/Binary_Tree/ZigZagTraversal.java
@@ -0,0 +1,52 @@
+package com.anurag.Binary_Tree;
+
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+public class ZigZagTraversal {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null) return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean flag = false;
+        while(!q.isEmpty()){
+            int level = q.size();
+            List<Integer> subList = new ArrayList<>();
+            for (int i = 0; i < level; i++) {
+                TreeNode node = q.poll();
+                if(node.left != null) q.offer(node.left);
+                if(node.right != null) q.offer(node.right);
+                if(!flag){
+                    subList.add(node.val);
+                } else{
+                    subList.add(0, node.val);
+                }
+            }
+            ans.add(subList);
+            flag = !flag;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+
+    }
+}
