2d6182beb8e0525e98b45931428c49e22eb8b0d5
Anuragkumarsah <asah2307@gmail.com>
2022-07-25 19:48:51 +0530

    Add all codes

--- anurag/Binary_Tree/PreOrder_Traversal.java
+++ anurag/Binary_Tree/PreOrder_Traversal.java
@@ -4,25 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+
+
 public class PreOrder_Traversal {
-    public class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode() {
-        }
-
-        TreeNode(int val) {
-            this.val = val;
-        }
-
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
-    }
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
