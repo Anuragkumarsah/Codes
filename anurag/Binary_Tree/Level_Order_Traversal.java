2b2b8f5a710fd542f2fa5e98b76b5d4096c57147
Anuragkumarsah <asah2307@gmail.com>
2022-07-28 13:41:13 +0530

    Add all codes

--- anurag/Binary_Tree/Level_Order_Traversal.java
+++ anurag/Binary_Tree/Level_Order_Traversal.java
@@ -1,25 +1,11 @@
 package com.anurag.Binary_Tree;
 
-import javax.swing.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class Level_Order_Traversal {
-    public class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
-
     public static List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> ans = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
@@ -28,7 +14,9 @@ public class Level_Order_Traversal {
        while(!q.isEmpty()){
            int levelNum = q.size();
            List<Integer> sublist = new LinkedList<>();
+
            for (int i = 0; i < levelNum; i++) {
+               assert q.peek()!=null;
                if(q.peek().left!=null) q.add(q.peek().left);
                if (q.peek().right!=null) q.add(q.peek().right);
                sublist.add(q.poll().val);
